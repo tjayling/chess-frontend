de1fc8d308746e35b5f13e02f6f83b43a4ff9474
thomas-ayling <thomas.ayling@ecs.co.uk>
2023-04-18 16:41:09 +0100

    Reducers added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import './App.css';
 import ChessUi from './chess/ChessUi';
+import PhoneSlider from './chess/PhoneSlider';
+import Index from "./pages";
+import {Provider} from "react-redux";
+import {store} from "./store";
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <ChessUi/>
-      </header>
-    </div>
-  );
+    return (
+        <Provider store={store}>
+            <div className='App'>
+                <header className='App-header'>
+                    <Index/>
+                    {/*<PhoneSlider/>*/}
+                </header>
+            </div>
+        </Provider>
+    );
 }
 
 export default App;
